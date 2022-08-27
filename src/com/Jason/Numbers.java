454cacf2129a85cbded79b1842b8ecdb55c387a4
jasonjkaminsky <jasonjkaminsky@gmail.com>
2022-08-27 18:35:08 -0500

    changed code for odd numbers to even numbers

--- src/com/Jason/Numbers.java
+++ src/com/Jason/Numbers.java
@@ -3,7 +3,7 @@ package com.Jason;
 public class Numbers {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i = i + 2) {
+        for (int i = 2; i <= 100; i = i + 2) {
             System.out.println(i);
         }
 
